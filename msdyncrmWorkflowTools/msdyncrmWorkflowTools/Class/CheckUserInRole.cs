f1f83c46256121e02b4df11f6220fa09d2f9f37d
demian_rasko_cp <demianrasko@users.noreply.github.com>
2016-04-02 09:41:48 +0000

    1.0.18.3 - Fixed issue in CheckUserInRole. Now selecting the Security Role from the parent BU, it checks is you have the role correctly in all BUs

--- msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/CheckUserInRole.cs
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/CheckUserInRole.cs
@@ -29,98 +29,74 @@ namespace msdyncrmWorkflowTools
             Common objCommon = new Common(executionContext);
             objCommon.tracingService.Trace("Load CRM Service from context --- OK");
             #endregion
-
+             
             #region "Read Parameters"
             EntityReference roleReference = this.Role.Get(executionContext);
 
             objCommon.tracingService.Trace(String.Format("RoleId: {0} ", roleReference.Id.ToString()));
             #endregion
 
-            QueryExpression query = new QueryExpression
-            {
-                EntityName = "role",
-                ColumnSet = new ColumnSet("roleid"),
-                Criteria = new FilterExpression
-                {
-                    Conditions =
-                {
+           
 
-                    new ConditionExpression
-                    {
-                        AttributeName = "roleid",
-                        Operator = ConditionOperator.Equal,
-                        Values = {roleReference.Id}
-                    }
+            Console.WriteLine("Checking association between user and role.");
+            // Establish a SystemUser link for a query.
+            LinkEntity systemUserLink = new LinkEntity()
+            {
+                LinkFromEntityName = "systemuserroles",
+                LinkFromAttributeName = "systemuserid",
+                LinkToEntityName = "systemuser",
+                LinkToAttributeName = "systemuserid",
+                LinkCriteria =
+            {
+                Conditions =
+                {
+                    new ConditionExpression(
+                        "systemuserid", ConditionOperator.Equal, objCommon.context.InitiatingUserId)
                 }
-                }
+            }
             };
 
-            EntityCollection givenRoles = objCommon.service.RetrieveMultiple(query);
-
-            if (givenRoles.Entities.Count > 0)
+            // Build the query.
+                
+            QueryExpression linkQuery = new QueryExpression()
             {
-                Entity givenRole = givenRoles.Entities[0].ToEntity<Entity>();
-
-                Console.WriteLine("Role {0} is retrieved.", givenRole);
-
-                Console.WriteLine("Checking association between user and role.");
-                // Establish a SystemUser link for a query.
-                LinkEntity systemUserLink = new LinkEntity()
+                EntityName = "role",
+                ColumnSet = new ColumnSet("parentrootroleid"),
+                LinkEntities =
+            {
+                new LinkEntity()
                 {
-                    LinkFromEntityName = "systemuserroles",
-                    LinkFromAttributeName = "systemuserid",
-                    LinkToEntityName = "systemuser",
-                    LinkToAttributeName = "systemuserid",
-                    LinkCriteria =
-                {
-                    Conditions =
-                    {
-                        new ConditionExpression(
-                            "systemuserid", ConditionOperator.Equal, objCommon.context.InitiatingUserId)
-                    }
+                    LinkFromEntityName = "role",
+                    LinkFromAttributeName = "roleid",
+                    LinkToEntityName = "systemuserroles",
+                    LinkToAttributeName = "roleid",
+                    LinkEntities = {systemUserLink}
                 }
-                };
-
-                // Build the query.
-                QueryExpression linkQuery = new QueryExpression()
+            },
+                Criteria =
+            {
+                Conditions =
                 {
-                    EntityName = "role",
-                    ColumnSet = new ColumnSet("roleid"),
-                    LinkEntities =
-                {
-                    new LinkEntity()
-                    {
-                        LinkFromEntityName = "role",
-                        LinkFromAttributeName = "roleid",
-                        LinkToEntityName = "systemuserroles",
-                        LinkToAttributeName = "roleid",
-                        LinkEntities = {systemUserLink}
-                    }
-                },
-                    Criteria =
-                {
-                    Conditions =
-                    {
-                        new ConditionExpression("roleid", ConditionOperator.Equal, givenRole.Id)
-                    }
+                    new ConditionExpression("parentrootroleid", ConditionOperator.Equal, roleReference.Id.ToString())
                 }
-                };
-
-                // Retrieve matching roles.
-                EntityCollection matchEntities = objCommon.service.RetrieveMultiple(linkQuery);
-
-                // if an entity is returned then the user is a member
-                // of the role
-                Boolean UserInRole = (matchEntities.Entities.Count > 0);
-
-                if (UserInRole)
-                    Console.WriteLine("User do not belong to the role.");
-                else
-                    Console.WriteLine("User belong to this role.");
-
-                this.isUserInRole.Set(executionContext, UserInRole);
-
             }
+            };
+
+            // Retrieve matching roles.
+            EntityCollection matchEntities = objCommon.service.RetrieveMultiple(linkQuery);
+
+            // if an entity is returned then the user is a member
+            // of the role
+            Boolean UserInRole = (matchEntities.Entities.Count > 0);
+
+            if (UserInRole)
+                Console.WriteLine("User do not belong to the role.");
+            else
+                Console.WriteLine("User belong to this role.");
+
+            this.isUserInRole.Set(executionContext, UserInRole);
+
+            
 
         }
     }
