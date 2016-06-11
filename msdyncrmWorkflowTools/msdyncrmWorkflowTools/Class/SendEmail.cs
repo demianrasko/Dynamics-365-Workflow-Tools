a56988aa1f840686a181cfda1b3899ac2263d9c2
demian_rasko_cp <SND\demian_rasko_cp>
2016-06-11 21:38:46 +0000

    1.0.27.0 - Added Send Email feature

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/SendEmail.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace msdyncrmWorkflowTools.Class
+{
+    public class SendEmail : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Email to send")]
+        [ReferenceTarget("email")]
+        public InArgument<EntityReference> SourceEmail
+        { get; set; }
+
+        [Output("Email Subject")]
+        public OutArgument<string> Subject { get; set; }
+
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+            EntityReference email = SourceEmail.Get(executionContext);
+
+            #endregion
+
+            #region "SendEmail Execution"
+
+            
+            SendEmailResponse ser = objCommon.service.Execute(
+                new SendEmailRequest()
+                {
+                    EmailId = email.Id,
+                    IssueSend = true
+                }
+              ) as SendEmailResponse;
+
+            if (ser != null)
+            {
+                Subject.Set(executionContext, ser.Subject);
+            }
+
+            #endregion
+
+        }
+    }
+}
