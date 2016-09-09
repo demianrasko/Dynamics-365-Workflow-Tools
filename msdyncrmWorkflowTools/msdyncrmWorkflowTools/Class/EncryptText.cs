71877251cbce8e06a4fa49b891b7e45cb41e66b9
demian_rasko_cp <demianrasko@users.noreply.github.com>
2016-09-09 21:24:37 +0000

    1.0.33.0 - "Get Initiating User" & "Encrypt Text" features

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/EncryptText.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msdyncrmWorkflowTools
+{
+    public class EncryptText : CodeActivity
+    {
+        #region "Parameter Definition"
+        [RequiredArgument]
+        [Input("Text to Encrypt")]
+        [Default("")]
+        public InArgument<String> TexttoEncrypt { get; set; }
+
+      
+
+
+        [Output("MD5 Hash Value")]
+        public OutArgument<String> MD5HashValue { get; set; }
+
+
+       
+        
+        #endregion
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+            String _TexttoEncrypt = this.TexttoEncrypt.Get(executionContext);
+           
+
+            objCommon.tracingService.Trace(String.Format("_TexttoEncrypt: {0} ",_TexttoEncrypt));
+            #endregion
+
+
+            #region "Encryption Execution"
+            string _MD5HashValue = MD5Hash(_TexttoEncrypt);
+            this.MD5HashValue.Set(executionContext, _MD5HashValue);
+               
+            #endregion
+
+        }
+        public string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            //get hash result after compute it
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits
+                //for each byte
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
+    }
+}
---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/GetInitiatingUser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+namespace msdyncrmWorkflowTools.Class
+{
+    public class GetInitiatingUser : CodeActivity
+    {
+       
+
+        [Output("Initiating User")]
+        [ReferenceTarget("systemuser")]
+        public OutArgument<EntityReference> InitiatingUser
+        {
+            get;
+            set;
+        }
+
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            var context = executionContext.GetExtension<IWorkflowContext>();
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            InitiatingUser.Set(executionContext, new EntityReference("systemuser", context.InitiatingUserId));
+
+        }
+
+     
+
+    }
+}
