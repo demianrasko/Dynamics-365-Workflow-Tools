900e35484f3b39b00c8f885b622df1933cabe72f
Brent Howard <go4brent@gmail.com>
2018-12-07 19:54:35 +1100

    Added Is Member Of Team

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/IsMemberOfTeam.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xrm.Sdk.Discovery;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;
+
+
+
+namespace msdyncrmWorkflowTools.Class
+{
+    public class IsMemberOfTeam : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("User")]
+        [ReferenceTarget("systemuser")]
+        public InArgument<EntityReference> User { get; set; }
+
+        [RequiredArgument]
+        [Input("Team")]
+        [ReferenceTarget("team")]
+        public InArgument<EntityReference> Team { get; set; }
+
+        [Output("Result")]
+        public OutArgument<bool> Result { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+
+           
+            EntityReference user = this.User.Get(executionContext);
+            EntityReference team = this.Team.Get(executionContext);
+
+            #endregion
+
+            #region "Is user member of team"
+            // Id of the specific Team
+            Guid teamId = team.Id;
+            // Id of the specific User
+            Guid userId = user.Id;
+
+            var query = new QueryExpression
+            {
+                EntityName = "teammembership",
+                ColumnSet = new ColumnSet("systemuserid", "teamid"),
+                Criteria =
+                        {
+                            Conditions =
+                            {
+                                new ConditionExpression ("systemuserid", ConditionOperator.Equal, userId),
+                                new ConditionExpression ("teamid", ConditionOperator.Equal, teamId)
+                            }
+                        }
+            };
+            //get the results
+            EntityCollection retrievedUsers = objCommon.service.RetrieveMultiple(query);
+
+            #endregion
+            #region "Check if there are an returned users"
+            
+            this.Result.Set(executionContext, retrievedUsers.Entities.Count > 0);
+            
+            #endregion
+
+
+
+        }
+    }
+}
