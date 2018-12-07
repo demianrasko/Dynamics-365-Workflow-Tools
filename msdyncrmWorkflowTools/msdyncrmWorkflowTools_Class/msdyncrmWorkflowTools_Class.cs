fb5bcc2c31ddec2676fea7467ca6240744288dd6
Brent Howard <go4brent@gmail.com>
2018-12-07 14:42:46 +1100

    Added Get App Module ID and Get App Record URL

--- msdyncrmWorkflowTools/msdyncrmWorkflowTools_Class/msdyncrmWorkflowTools_Class.cs
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools_Class/msdyncrmWorkflowTools_Class.cs
@@ -310,6 +310,32 @@ namespace msdyncrmWorkflowTools
             return objectId;
         }
 
+        public string GetAppModuleId(string appModuleUniqueName)
+        {
+            var query = new QueryExpression
+            {
+                EntityName = "appmodule",
+                ColumnSet = new ColumnSet("appmoduleid", "uniquename"),
+                Criteria =
+                        {
+                            Conditions =
+                            {
+                                new ConditionExpression ("uniquename", ConditionOperator.Equal, appModuleUniqueName)
+                            }
+                        }
+            };
+
+            var appmodules = service.RetrieveMultiple(query).Entities;
+            return appmodules.First()["appmoduleid"].ToString();
+        }
+
+        public string GetAppRecordUrl(string recordUrl, string appModuleUniqueName)
+        {
+            string appModuleId = GetAppModuleId(appModuleUniqueName);
+
+            return recordUrl + "&appid=" + appModuleId;
+        }
+
         public bool DateFunctions(DateTime date1, DateTime date2, ref TimeSpan difference,
             ref int DayOfWeek, ref int DayOfYear, ref int Day, ref int Month, ref int Year, ref int WeekOfYear)
         {
