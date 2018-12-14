f0d5c9c07f4dae30d227b96fa562cfee9a0f2e47
Demian Adolfo Raschkovan <mvp@demianrasko.com>
2018-12-14 22:21:10 +0100

    Added new stuff

--- docs/CountChildEntityRecords.md
+++
@@ -1,4 +0,0 @@
-This action is used to count the child records. A common example of this is to count how many contact record exists for a specific account record.
-
-![](CountChildEntityRecords.gif)
-
---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/GetAppModuleID.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace msdyncrmWorkflowTools
+{
+    public class GetAppModuleID : CodeActivity
+    {
+        #region "Parameter Definition"
+        [RequiredArgument]
+        [Input("Application Unique Name")]
+        [Default("")]
+        public InArgument<String> AppModuleUniqueName { get; set; }
+
+        [Output("App Module ID")]
+        public OutArgument<string> AppModuleId { get; set; }
+
+        #endregion
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+
+            #region "Load CRM Service from context"
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+            String appModuleUniqueName = this.AppModuleUniqueName.Get(executionContext);
+            #endregion
+
+            msdyncrmWorkflowTools_Class commonClass = new msdyncrmWorkflowTools_Class(objCommon.service, objCommon.tracingService);
+            
+            string appModuleId = commonClass.GetAppModuleId(appModuleUniqueName);
+                
+            this.AppModuleId.Set(executionContext, appModuleId);
+
+        }
+        
+
+    }
+}
