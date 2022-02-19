2b75487be6f072fc13f57d4451b9bca06f95f2fe
Demian Raschkovan <demian_rasko@yahoo.com>
2022-02-19 14:52:14 +0100

    New Features 1.0.62.0: CreateOpportunityProduct ExecuteWorkflowForRecordsinQuery DistributeWFActivityManyToMany DistributeWFActivityOneToMany UpdateProductQuoteValue UpdateQuoteValue

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/CreateQuoteFromOpportunity.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace msdyncrmWorkflowTools
+{
+    public class CreateQuoteFromOpportunity : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Opportunity")]
+        [ReferenceTarget("opportunity")]
+        public InArgument<EntityReference> Opportunity { get; set; }
+
+        [Output("Quote")]
+        [ReferenceTarget("quote")]
+        public OutArgument<EntityReference> Quote { get; set; }
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+            EntityReference opportunity = this.Opportunity.Get(executionContext);
+            #endregion
+            // Convert the opportunity to a quote
+
+            var genQuoteFromOppRequest = new GenerateQuoteFromOpportunityRequest
+            {
+                OpportunityId = opportunity.Id,
+                ColumnSet = new ColumnSet("quoteid", "name")
+            };
+            var genQuoteFromOppResponse = (GenerateQuoteFromOpportunityResponse)objCommon.service.Execute(genQuoteFromOppRequest);
+            Entity quote = genQuoteFromOppResponse.Entity;
+            EntityReference _quote = new EntityReference(quote.LogicalName, quote.Id);
+
+            this.Quote.Set(executionContext, _quote);
+        }
+    }
+}
---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/WinQuote.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace msdyncrmWorkflowTools
+{
+    public class WinQuote : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Quote")]
+        [ReferenceTarget("quote")]
+        public InArgument<EntityReference> Quote { get; set; }
+
+        [Input("Message")]
+        public InArgument<string> Message { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+            #endregion
+
+            #region "Read Parameters"
+            EntityReference quote = this.Quote.Get(executionContext);
+            objCommon.tracingService.Trace(String.Format("quote: {0} ", quote.Id.ToString()));
+
+            string message = this.Message.Get(executionContext);
+            objCommon.tracingService.Trace(String.Format("Discountamount: {0} ", message.ToString()));
+            #endregion
+
+            Entity quoteclose = new Entity("quoteclose");
+            WinQuoteRequest winQuoteRequest = new WinQuoteRequest();
+            quoteclose.Attributes.Add("subject", message);
+            quoteclose.Attributes.Add("quoteid", quote);
+            winQuoteRequest.QuoteClose = quoteclose;
+            winQuoteRequest.Status = new OptionSetValue(-1);
+            objCommon.service.Execute(winQuoteRequest);
+        }
+    }
+}
